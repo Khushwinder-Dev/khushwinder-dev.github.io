141fc4e161dbd051ce233580f90d7c19a12de2ff
Khushwinder <154021262+Khushwinder-Dev@users.noreply.github.com>
2026-01-16 06:53:54 +0530

    Profile Fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,24 @@
 // Example JavaScript file
-console.log('Custom JS loaded successfully!');
+console.log("Custom JS loaded successfully!");
 
 // Optional: simple example interaction
-document.addEventListener('DOMContentLoaded', () => {
-  const heading = document.querySelector('h1');
-  if (heading) heading.style.color = '#0078ff';
+document.addEventListener("DOMContentLoaded", () => {
+  const heading = document.querySelector("h1");
+  if (heading) heading.style.color = "#0078ff";
+});
+
+new Typed("#typed-text", {
+  strings: [
+      "Full Stack Developer",
+      "Shopify Expert",
+      "Wordpress Expert",
+      "Ecommerce Expert"
+    ],
+  typeSpeed: 80,
+  backSpeed: 20,
+  backDelay: 3500,
+  loop: true,
+  smartBackspace: true,
+  showCursor: true,
+  cursorChar: "|",
 });
