d5d39c3922f965fa3992d9bbba65a0b789107b05
Khushwinder <154021262+Khushwinder-Dev@users.noreply.github.com>
2026-06-24 16:54:31 +0530

    Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/shopify-developer-portfolio',
-  assetPrefix: '/shopify-developer-portfolio/',
   typescript: {
     ignoreBuildErrors: true,
   },
--- out/_next/static/97YubdJHF06ZGy722zGK1/_buildManifest.js
+++ out/_next/static/97YubdJHF06ZGy722zGK1/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/shopify-developer-portfolio//_next/:path+",
-        "destination": "/shopify-developer-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
