92af3cd3b23955de6e5e1137c0bff57367c9d063
Khushwinder <154021262+Khushwinder-Dev@users.noreply.github.com>
2026-06-24 15:53:09 +0530

    Next-js version

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/shopify-developer-portfolio',
+  assetPrefix: '/shopify-developer-portfolio/',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
